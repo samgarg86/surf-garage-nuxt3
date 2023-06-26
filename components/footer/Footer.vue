188c0e1e9e25f3605f51bf4ea163f0907ad45b10
Sam Garg <s.garg@astoundcommerce.com>
2023-06-26 21:56:14 +0100

    Footer grid css

--- components/footer/Footer.vue
+++ components/footer/Footer.vue
@@ -1,16 +1,20 @@
 <template>
-  <section id="contact" class="section contact max-w-full">
-    <h2 class="section__title font-metalsmith py-5 text-center">
-      <span class="color-black px-1 py-0.5">{{ $t("contact.title") }}</span>
-    </h2>
+  <section id="contact" class="section footer max-w-full">
+<!--    <h2 class="section__title font-metalsmith py-5 text-center">-->
+<!--      <span class="color-black px-1 py-0.5">{{ $t("contact.title") }}</span>-->
+<!--    </h2>-->
 <!--    <LazyInstaFeed/>-->
     <!--      <pre class="text-white">{{ siteNav?.boardStorageMenu }}</pre>-->
     <!--      <pre class="text-white">{{ siteNav?.surfArtMenu }}</pre>-->
-    <div class="px-3 pb-2 text-center md:text-left md:p-5 md:grid grid-cols-footer gap-5 ">
-      <LazyFooterNav :title="$t('nav.storage')" to="/" :nav="siteNav?.boardStorageMenu || {}"/>
-      <LazyFooterNav title="Surf Art" to="/" :nav="siteNav?.surfArtMenu || {}"/>
-      <LazyContactLinks class="contact__links font-metalsmith lowercase mr-5" theme="light"/>
-      <div class="contact__form w-full font-metalsmith lowercase">
+    <div class="footer__content text-center md:text-left px-2 py-5 lg:py-10 lg:px-6">
+      <LazyFooterNav
+        class="footer__nav1"
+        :title="$t('nav.storage')" to="/" :nav="siteNav?.boardStorageMenu || {}"/>
+      <LazyFooterNav
+        class="footer__nav2"
+        title="Surf Art" to="/" :nav="siteNav?.surfArtMenu || {}"/>
+      <LazyContactLinks class="footer__links font-metalsmith lowercase" theme="light"/>
+      <div class="footer__form w-full font-metalsmith lowercase">
         <LazyFooterContactForm />
       </div>
     </div>
@@ -21,8 +25,58 @@ const { siteNav } = useSiteNav()
 </script>
 
 <style lang="postcss">
-.contact {
+.footer {
   background: url('@/assets/images/wave-bg.jpg') no-repeat center center;
   background-size: cover;
+
+  &__nav1 {
+    grid-area: nav1;
+  }
+
+  &__nav2 {
+    grid-area: nav2;
+  }
+
+  &__links {
+    grid-area: links;
+    //margin: 0 auto;
+
+    @media screen(md) {
+      padding: 0;
+    }
+  }
+
+  &__form {
+    grid-area: form;
+
+    @media screen(md) {
+      max-width: 80rem;
+    }
+
+    @media screen(lg) {
+      max-width: 60rem;
+    }
+  }
+
+  &__content {
+    display: grid;
+    grid-gap: 3rem;
+
+    grid-template-areas:
+      "links links"
+      "form form"
+      "nav1 nav2";
+
+    @media screen(md) {
+      grid-template-areas:
+        "links links nav1 nav2"
+        "form form form form";
+    }
+
+    @media screen(lg) {
+      grid-template-areas:
+        "nav1 nav2 links form form"
+    }
+  }
 }
 </style>
