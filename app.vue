f03de8ab65038c1fc7e43de39f4b67ea4356fc00
Sam Garg <s.garg@astoundcommerce.com>
2023-09-20 16:06:05 +0100

    Google site verification

--- app.vue
+++ app.vue
@@ -21,6 +21,7 @@ useHead({
     { charset: 'utf-8' },
     { name: 'viewport', content: 'width=device-width, initial-scale=1' },
     { 'http-equiv': 'accept-ch', content: 'width' },
+    { name: 'google-site-verification', content: 'zFkorbR5IM4CttFNPeqBdq-iNibbkw_c032veRnXwE4' },
     ...(i18nHead.value.meta || [])
   ],
   link: [
