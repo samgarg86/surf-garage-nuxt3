63cc2035482661fc1b63037f2a788439aecbd72a
Sam Garg <s.garg@astoundcommerce.com>
2023-09-28 14:38:52 +0100

    Stop showing same poster again and again

--- components/art/MasonryPosterGallery.vue
+++ components/art/MasonryPosterGallery.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 mb-1 md:mb-2 gap-1 md:gap-2">
+  <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 mb-1 md:mb-2 gap-1 md:gap-4">
     <ArtMasonryPoster
-        v-for="{id, title, image, tags} in [...posters, ...posters, ...posters, ...posters, ...posters]"
+        v-for="{id, title, image, tags} in [...posters]"
         :key="id"
         :id="id"
         :url="image.url"
