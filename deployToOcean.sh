de950fcd7d8d8dbd560e6a16027c381170974349
Sam Garg <s.garg@astoundcommerce.com>
2023-11-10 16:09:57 +0000

    Deploy script updated

--- deployToOcean.sh
+++ deployToOcean.sh
@@ -1,7 +1,9 @@
 ssh root@174.138.103.192 <<EOF
 pm2 kill
 cd surf-garage-nuxt3
-git pull && yarn install
+git switch main
+git pull
+yarn install
 yarn build:art
 yarn build:board
 pm2 start ecosystem.config.cjs
