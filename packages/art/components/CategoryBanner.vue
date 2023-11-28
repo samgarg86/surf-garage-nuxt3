f80a861c9ab677f2d7921c3181f5d47ec9016c4a
Sam Garg <s.garg@astoundcommerce.com>
2023-11-28 16:42:57 +0000

    Padding adjusted

--- packages/art/components/CategoryBanner.vue
+++ packages/art/components/CategoryBanner.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="py-6 text-center max-w-screen-md mx-auto">
+  <div class="py-4 md:py-6 text-center max-w-screen-md mx-auto">
     <h1 class="text-[8rem] md:text-[10rem] font-avenir mb-2 leading-none overflow-hidden overflow-ellipsis">{{ title }}</h1>
     <h2 v-if="description" class="text-2">{{ description }}</h2>
   </div>
