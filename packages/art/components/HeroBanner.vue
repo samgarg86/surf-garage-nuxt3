7fee7fa0e08ee208cd671b4c180a22046f2572dd
Sam Garg <s.garg@astoundcommerce.com>
2023-12-31 18:44:10 +0000

    Hero banner width updated

--- packages/art/components/HeroBanner.vue
+++ packages/art/components/HeroBanner.vue
@@ -2,7 +2,7 @@
   <div class="art-hero-banner -mx-1 md:-mx-2 px-2 py-6 md:py-9 text-center text-white"
        :style="{ '--art-hero-bg': `url(${backgroundImage.fields.file.url}?w=1200&fm=webp)` }"
   >
-    <div class="max-w-[80rem] mx-auto">
+    <div class="max-w-[70rem] mx-auto">
       <h1 class="text-[4rem] md:text-[7rem] font-avenir font-medium leading-tight tracking-tight">{{ title }}</h1>
       <h2 v-if="description" class="mt-2 leading-relaxed mb-4 text-1.8 md:text-2">{{ description }}</h2>
       <nuxt-link
