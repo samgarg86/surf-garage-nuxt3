7fcf8af047f93f20f8baa807ffac6a645493bb58
Sam Garg <s.garg@astoundcommerce.com>
2024-01-02 17:36:31 +0000

    /en SEO permanent redirects

---
+++ packages/art/pages/en/[...enSlug].vue
@@ -0,0 +1,12 @@
+<template>
+  Redirect to surfgarage.art english
+  <pre>{{pageSlug}}</pre>
+</template>
+<script setup lang="ts">
+const { params: { enSlug } } = useRoute()
+const pageSlug = enSlug?.[0] ? enSlug.join('/') : ''
+navigateTo(`https://surfgarage.art/${pageSlug}`, {
+  external: true,
+  redirectCode: 301
+})
+</script>
