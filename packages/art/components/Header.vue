525f0d99e384491cff02d3bf0f562af85a0183ff
Gesh <Geshaaa91@gmail.com>
2024-01-31 15:46:37 +0200

    Lang selector in header

--- packages/art/components/Header.vue
+++ packages/art/components/Header.vue
@@ -2,7 +2,7 @@
   <header class="fixed z-10 left-0 top-0 w-full text-center py-1.5 bg-black">
     <NuxtLink :to="localeRoute('/')" class="font-avenir font-extrabold text-2 md:text-[2.3rem] text-white">surf garage art co.</NuxtLink>
     <div class="flex absolute right-1 top-2 md:top-1 md:right-2 md:items-start">
-      <LazyLanguageSwitcher :showLanguageName="false" @click="toggleLangExpanded" theme="dark" :class="['header-lang', { 'isExpanded': langExpanded } ]"/>
+      <LazyLanguageSwitcher :showLanguageName="false" @click="toggleLangExpanded" theme="dark" mode="dropdown" :class="['header-lang', { 'isExpanded': langExpanded } ]"/>
       <button class="snipcart-checkout flex items-center ml-1 md:mt-1">
         <SvgoCart class="w-2 h-2 text-white"/>
         <div class="snipcart-items-count text-xs text-white -translate-y-1"/>
@@ -20,7 +20,7 @@ const toggleLangExpanded = () => {
 </script>
 <style>
 .header-lang {
-  @apply relative overflow-hidden w-5 h-4.5 text-1.8 mobile:hidden;
+  @apply relative overflow-hidden w-6 h-4.5 text-1.8 mobile:hidden;
 
   &.isExpanded {
     @apply h-9 rounded-lg;
@@ -37,22 +37,26 @@ const toggleLangExpanded = () => {
         }
       }
     }
+
+    .language-switcher__chevron {
+      @apply text-black -rotate-180;
+    }
   }
 
   .language-switcher {
     @apply absolute w-full flex-col-reverse gap-0.5;
 
     a {
-      @apply px-1.5 py-1 leading-tight;
+      @apply pl-1.5 pr-2.5 py-1 leading-tight;
     }
   }
 
-  .language-switcher__separator {
-    @apply hidden;
+  .language-switcher__chevron {
+    @apply transition-all;
   }
 
-  .router-link-active {
-    @apply order-1;
+  a.router-link-active {
+    @apply order-1 no-underline;
   }
 }
 </style>
