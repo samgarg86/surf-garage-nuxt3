2700aa1b53fbe3226a4dd6a4cc3d6c9280780fb5
Sam Garg <s.garg@astoundcommerce.com>
2026-03-10 15:45:46 +0000

    Fix ref issue on posters page

--- packages/art/pages/poster/[id].vue
+++ packages/art/pages/poster/[id].vue
@@ -1,65 +1,102 @@
 <template>
-  <Breadcrumbs class="h-[55px]"/>
-  <div class="grid md:grid-cols-[55%_auto] gap-1 md:gap-6 mb-2 max-w-screen-container mx-auto">
+  <Breadcrumbs class="h-[55px]" />
+  <div
+    class="grid md:grid-cols-[55%_auto] gap-1 md:gap-6 mb-2 max-w-screen-container mx-auto"
+  >
     <section class="mobile:-mx-1">
       <PdpGalleryPosters :images="images" />
-    </section >
+    </section>
     <section class="md:mr-2">
       <div class="md:mt-3 mb-1">
         <h1 class="text-3xl md:text-4xl font-primary">{{ title }}</h1>
 
-        <LazyArtistPlaceTag v-if="tags.artist" class="mb-1" :artist="tags.artist"/>
-        <h2 v-if="description" class="text-2">{{description}}</h2>
+        <LazyArtistPlaceTag
+          v-if="tags.artist"
+          class="mb-1"
+          :artist="tags.artist"
+        />
+        <h2 v-if="description" class="text-2">{{ description }}</h2>
 
         <ul v-if="tags.page?.length" class="tags mb-1 text-right md:text-left">
           <template v-for="tag in tags.page" :key="tag">
-            <li v-if="tag !== 'Home'" class="inline-block text-sm px-1 leading-9 mr-1 last:mr-0 bg-grey-10">
-              <NuxtLink :to="localePath(collectionSlug(tag))">{{ tag.name }}</NuxtLink>
+            <li
+              v-if="tag !== 'Home'"
+              class="inline-block text-sm px-1 leading-9 mr-1 last:mr-0 bg-grey-10"
+            >
+              <NuxtLink :to="localePath(collectionSlug(tag))">{{
+                tag.name
+              }}</NuxtLink>
             </li>
           </template>
         </ul>
-
       </div>
 
-      <div class="text-2xl mb-1 font-primary font-medium">€{{pricing[size]}}</div>
-      <SizeSelector v-model="size" class="mb-1"/>
+      <div class="text-2xl mb-1 font-primary font-medium">
+        €{{ pricing[size] }}
+      </div>
+      <SizeSelector v-model="size" class="mb-1" />
       <AddToCart
-          :id="`${id}`"
-          :price="basePrice"
-          :title="title"
-          :decription="size"
-          :image="`${images[0].url}?w=600`"
-          :sizes="priceOptions(priceEntries)"
-          :selectedSize="size"
-          :url="validateUrl(id, host, 'posters')"
-          :ecomDisabled="ecomDisabled"
+        :id="`${id}`"
+        :price="basePrice"
+        :title="title"
+        :decription="size"
+        :image="`${images[0].url}?w=600`"
+        :sizes="priceOptions(priceEntries)"
+        :selectedSize="size"
+        :url="validateUrl(id, host, 'posters')"
+        :ecomDisabled="ecomDisabled"
       />
       <Accordion class="mt-3 md:mt-5">
-        <AccordionItem :title="$t('art.accordion.materials.title')" title-class="text-black font-primary" class="border-t">
-          <p class="text-sm text-justify">{{$t('art.accordion.materials.descPosters')}}</p>
-        </AccordionItem>
-        <AccordionItem :title="$t('art.accordion.shipping.title')" title-class="text-black font-primary" class="border-t">
-          <p class="text-sm text-justify mb-1">{{$t('art.accordion.shipping.line1')}}</p>
-          <p class="text-sm text-justify mb-1">{{$t('art.accordion.shipping.line2')}}</p>
-          <p class="text-sm text-justify font-bold">{{$t('art.accordion.shipping.free')}}</p>
-        </AccordionItem>
-        <AccordionItem :title="$t('art.accordion.contact.title')" title-class="text-black font-primary" class="border-t">
+        <AccordionItem
+          :title="$t('art.accordion.materials.title')"
+          title-class="text-black font-primary"
+          class="border-t"
+        >
           <p class="text-sm text-justify">
-            {{$t('art.accordion.contact.desc')}} <ScrollTo to="contact" class="underline">{{$t('art.accordion.contact.link')}}</ScrollTo>
+            {{ $t('art.accordion.materials.descPosters') }}
+          </p>
+        </AccordionItem>
+        <AccordionItem
+          :title="$t('art.accordion.shipping.title')"
+          title-class="text-black font-primary"
+          class="border-t"
+        >
+          <p class="text-sm text-justify mb-1">
+            {{ $t('art.accordion.shipping.line1') }}
+          </p>
+          <p class="text-sm text-justify mb-1">
+            {{ $t('art.accordion.shipping.line2') }}
+          </p>
+          <p class="text-sm text-justify font-bold">
+            {{ $t('art.accordion.shipping.free') }}
+          </p>
+        </AccordionItem>
+        <AccordionItem
+          :title="$t('art.accordion.contact.title')"
+          title-class="text-black font-primary"
+          class="border-t"
+        >
+          <p class="text-sm text-justify">
+            {{ $t('art.accordion.contact.desc') }}
+            <ScrollTo to="contact" class="underline">{{
+              $t('art.accordion.contact.link')
+            }}</ScrollTo>
           </p>
         </AccordionItem>
       </Accordion>
     </section>
   </div>
   <LazySliderPosters
-      :title="$t('art.moreBy', {name: tags.artist.name})"
-      :tag="tags.artist.id"
-      class="mt-6"
-      order="created"
+    :title="$t('art.moreBy', { name: tags.artist.name })"
+    :tag="tags.artist.id"
+    class="mt-6"
+    order="created"
   />
 </template>
 <script setup>
-const { params: { id } } = useRoute()
+const {
+  params: { id }
+} = useRoute()
 const { getPoster } = useContentfulPosters()
 const localePath = useLocalePath()
 const host = useHost()
@@ -67,12 +104,18 @@ const { gtag } = useGtag()
 
 const { title, description, images, tags, specialPrice } = await getPoster(id)
 
-const { public: { priceTable: { posters } } } = useRuntimeConfig()
+const {
+  public: {
+    priceTable: { posters }
+  }
+} = useRuntimeConfig()
 const pricing = computed(() => specialPrice || posters)
 const priceEntries = computed(() => Object.entries(pricing.value))
 const size = ref('30x40')
-const basePrice = computed(() => priceEntries[0][1])
-const ecomDisabled = computed(() => tags?.settings.includes('settingEcomDisabled'))
+const basePrice = computed(() => priceEntries.value[0][1])
+const ecomDisabled = computed(() =>
+  tags?.settings.includes('settingEcomDisabled')
+)
 const seoDescription = ref('')
 const { t } = useI18n()
 
