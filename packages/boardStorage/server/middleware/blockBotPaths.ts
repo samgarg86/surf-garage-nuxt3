f9f3e9a5ff526da4504277e0893147533cb71828
Sam Garg <s.garg@astoundcommerce.com>
2026-03-20 13:34:45 +0000

    Now we have cloudflare

--- packages/boardStorage/server/middleware/blockBotPaths.ts
+++
@@ -1,7 +0,0 @@
-export default defineEventHandler((event) => {
-  const url = event.path
-  const botPatterns = ['/wp-', '/.env', '/.git', '/admin', '/phpmyadmin', '/xmlrpc']
-  if (botPatterns.some((p) => url.includes(p))) {
-    throw createError({ statusCode: 404, statusMessage: 'Not Found' })
-  }
-})
